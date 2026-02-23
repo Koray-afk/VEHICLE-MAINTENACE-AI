c868f44e5d3ee6b6cfa53894b7380e6241aba332
VIRAJ CHAFALE <virajchafle0007@gmail.com>
2026-02-23 17:42:48 +0530

    train_new_model

---
+++ main.py
@@ -0,0 +1,47 @@
+
+
+
+
+# import streamlit as st
+# st.title("Vehicle Maintenance Prediction")
+
+
+# st.header("Dashboard")
+
+# st.subheader("graph representation ")
+
+# st.text("accuracy of model")
+
+
+# st.success("Success")
+# st.warning("Warning")
+# st.info("Information")
+# st.error("Error")
+
+# # checkbox returns True if checkbox
+
+# if st.checkbox("select/unselect"):
+#   st.text("user select the checkbox")
+# else:
+#   st.text("User has not selected the checkbox")
+
+# # radio returns the text of the selected options
+# state=st.radio("What is your favorite color?",("Red","Green","Blue"))
+
+# if(state=='Green'):
+#   st.success("That is my favorite color as well")
+
+
+# # select box (dropdown)
+
+# occupation=st.selectbox("what do you do?",["Student","Vlogger","Enginner" ,"Doctor"])
+
+# st.text(f"Selected option is {occupation}")
+
+
+# if st.button("Example Button"):
+#   st.success("You clicked it")
+
+# # sidebar
+# st.sidebar.header("heading of sidebar")
+# st.sidebar.text("MLOPs by FunctionUP")
